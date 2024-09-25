6182714dcdbaab49379ee4e5d7a60010fdce36cb
Tony Nguyen <tnguyen411@gmail.com>
2024-09-25 15:20:10 -0700

    using npm packages bodyparser / morgan

--- middleware/index1.js
+++ middleware/index1.js
@@ -1,3 +1,4 @@
+import bodyParser from "body-parser";
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -5,6 +6,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true})); 
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(201);
+})
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
--- middleware/index2.js
+++ middleware/index2.js
@@ -1,7 +1,11 @@
 import express from "express";
+import morgan from "morgan"; 
 
 const app = express();
 const port = 3000;
+
+
+app.use(morgan("short")); 
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -10,3 +14,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
